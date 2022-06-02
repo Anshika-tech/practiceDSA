25238bca7510048939bcfc08117e95399b8d22ff
Anshika-tech <srivastavanshika10@gmail.com>
2022-06-02 20:30:39 +0530

    questions on Stack

--- src/Stack.java
+++ src/Stack.java
@@ -1,5 +1,5 @@
 //Implementation of Stack.
-public class Stack {
+/*public class Stack {
     int maxsize;
     long[] arr;
     int top;
@@ -36,4 +36,69 @@ public class Stack {
         }
 
     }
+}
+*/
+//Implementation of two stack in an array.
+public class Stack{
+    int size;
+    int top1,top2;
+    int arr[];
+    public Stack(int n){
+        size=n;
+        arr=new int[n];
+        top1=-1;
+        top2=size;
+    }
+    void push1(int x){
+        if(top1<top2-1){
+            top1++;
+            arr[top1]=x;
+        }
+        else{
+            System.out.println("Stack overflow");
+        }
+    }
+    void push2(int x){
+       if(top1<top2-1){
+           top2--;
+           arr[top2]=x;
+       }
+       else{
+           System.out.println("Stack overflow");
+       }
+    }
+    int pop1(){
+        if(top1>=0){
+            int x=arr[top1];
+            top1--;
+            return x;
+        }
+        else{
+            System.out.println("Stack underflow");
+        }
+        return 0;
+    }
+    int pop2(){
+        if(top2<size){
+            int x=arr[top2];
+            top2++;
+            return x;
+        }
+        else{
+            System.out.println("Stack underflow");
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Stack obj=new Stack(5);
+        obj.push1(5);
+        obj.push2(10);
+        obj.push2(15);
+        obj.push1(11);
+        obj.push2(7);
+        System.out.println("popped element from stack1 is "+obj.pop1());
+        System.out.println("popped element from stack2 is "+obj.pop2());
+    }
+
 }
